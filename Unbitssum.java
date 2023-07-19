97e64b0fdc967fcb337aca689bb766cdf628e6a8
Madanlatha <madanganesan08@gmail.com>
2023-07-19 10:31:33 +0530

    First commit

---
+++ Unbitssum.java
@@ -0,0 +1,32 @@
+package unbits;
+
+import java.util.Scanner;
+
+public class Unbitssum {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+        System.out.println(bitsetter(number,number2));
+    }
+
+    public static int bitsetter(int number, int number2) {
+        int x=number;
+        for(int i=0;i<number2;i++){
+            x>>=1;
+        }
+        if((x&1)==1){
+            return number^(1<<number2);
+        }
+        else{
+            return number;
+        }
+
+
+    }
+
+	}
+
+
